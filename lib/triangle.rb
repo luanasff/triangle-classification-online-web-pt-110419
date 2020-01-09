a516a06ee5b06fae496bd61d3f8ae1a8156a59b6
Luana Faria Lane <luanasfff@gmail.com>
2020-01-09 21:36:05 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,7 @@ class Triangle
   
   attr_accessor :side_1 , :side_2  , :side_3
   
-  def initialize(side_one ,side_two,side_three)
+  def initialize(side_1 ,side_2,side_3)
     @side_1 = side_1
     @side_2 = side_2
     @side_3 =side_3
